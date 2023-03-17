6b87d89ddb12736c00599b6f759983ae9fdfc868
Spencer Castro <rtdelacruz20@gmail.com>
2023-03-17 23:15:28 +0800

    nextjs config changes

--- next.config.js
+++ next.config.js
@@ -1,31 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
 module.exports = module.exports = {
-  future: {
-    webpack5: true,
-  },
   webpack: (config, options) =>
   {
       config.module.rules.push({
         test: /\.pdf$/i,
         type: 'asset/resource',
-        // use: [
-        //   {
-        //     loader: 'file-loader',
-        //   },
-        // ],
-        // generator: {
-        //   filename: `[name][ext]`
-        // },
-        // test: /\.pdf$/i,
-        // use: 'raw-loader',
       })
 
       return config
   },
-  nextConfig
+  // reactStrictMode: true,
+  // swcMinify: true,
 }
