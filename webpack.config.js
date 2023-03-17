927a8d48ec3823c0352fec5c078ec924d9b0608d
Spencer Castro <rtdelacruz20@gmail.com>
2023-03-17 22:21:26 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.pdf$/i,
+//         type: 'asset/resource',
+//         generator: {
+//           filename: `[name][ext]`
+//         }
+//       },
+//     ],
+//   },
+// };
